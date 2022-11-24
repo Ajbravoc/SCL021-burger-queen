276b4e0de066bfcc8d9d68e238b169276e5262fb
Anahí Bravo <108346279+Ajbravoc@users.noreply.github.com>
2022-11-24 12:35:42 -0300

    funcionalidad botones para pedir

--- scl021-burger-queen/src/routes/orderFoodView.jsx
+++ scl021-burger-queen/src/routes/orderFoodView.jsx
@@ -9,22 +9,47 @@ import { signOut } from "firebase/auth";
 import style from "./orderFoodView.module.css";
 import menu from "../menu/menu.json";
 
-
 export default function ChooseUsernameView() {
   const navigate = useNavigate();
-  const [state, setState] = useState(0);
+  const [state, setState] = useState(1);
   const [currentUser, setCurrentUser] = useState({});
+{/*}el valor de categoria es null*/}
+const [categoria, setCategoria] = useState("drink");
 
+
+
+{/*} creo un state nuevo, paso jugo como objeto, y usando map en el receipt */}
   function firebaseName(user) {
     //navigate ('/choose-username');
     setCurrentUser(user); //Setea el nombre de usuario loggeado
-    setState(3);
   }
   console.log(menu);
 
   const handleClick = () => {
     console.log("button clicked");
   };
+
+
+function Basket (props){
+  const { cartItems, onAdd, onRemove } = props;
+  const itemsPrice = cartItems.reduce ((a,c)=> a + c.qty * c.price,0 );
+  const taxPrice = itemsPrice * 0.14;
+  const shippingPrince = itemsPrice > 1000 ? 0 : 20;
+  const totalPrice = itemsPrice + taxPrice + shippingPrince;
+
+  return <aside div className ="block col-1">Basket
+  <h2>Cart Items</h2>
+  <div>
+
+  </div>
+  </aside>
+}
+
+
+
+
+
+
 
   //Está bien así?
   const logout = async () => {
@@ -47,8 +72,8 @@ function clock (){
 }
 setInterval(clock, 1000 )*/
 
-  if (state === 3) {
-    return (
+  return (
+    <>
       <div className={style.borderExt}>
         <div className={style.mainContainer}>
           <nav>
@@ -65,39 +90,101 @@ setInterval(clock, 1000 )*/
           </div>
           <div className={style.mainFoodContent}>
             <div className={style.mealBtns}>
-              <button onClick={handleClick} className={style.btnBreakfast}>
+
+              {/*}Con null desaparece la info en el contenedor*/}
+              <button onClick={() => {setState(1);}} className={style.btnBreakfast}>
                 Breakfast
               </button>
 
-              <button onClick={handleClick} className={style.btnLunch}>
+{/*}Aquí empieza todo xd*/}
+              <button onClick={() => {setState(2);}} className={style.btnLunch}>
                 Lunch/Dinner
               </button>
+              
             </div>
+{/*}Hace aparecer el json con tipo drink*/}
             <div className={style.typeOfMealsBtns}>
-              <button onClick={handleClick} className={style.btnDrinks}>
+              <button onClick={()=> {setCategoria("drink")}} className={style.btnDrinks}>
                 Drinks
               </button>
-              <button onClick={handleClick} className={style.btnSandwiches}>
+
+              {/*}Hace aparecer el json con tipo food*/}
+              <button onClick={()=> {setCategoria("food")}} className={style.btnSandwiches}>
                 Sandwiches
               </button>
             </div>
+
+     {/*}Si la categoria es = al nombre del json*/}       
+ {categoria === "breakfast" ? <>items breakfast </> : null}
+ {categoria === "lunch" ? <>items lunch </> : null}
+
             <div className={style.mainMenuContent}>
               <div className={style.menuContainer}>
-               <div className={style.holaContainer}>
-                 {menu.breakfast.map((item, indice,) => (
-                  <div className={style.foodPicsContainer}><p key={indice}>
-                    {item.name}
-                    <br/>
-                    <br/>
-                
-                <img src={item.image} alt="foto.png" />
-                    
-                     <div className={style.price}> {item.price}</div>
-                    </p></div>
-                ))}
-              </div></div>
+                <div className={style.holaContainer}>
+
+                  {state === 1 &&
+                    menu.breakfast.filter((item)=> item.type === categoria).map((item, indice) => (
+                      <div className={style.foodPicsContainer}>
+                        <p key={indice}>
+                          {item.name}
+                          <br />
+                          <br />
+
+                          <img src={item.image} alt="foto.png" />
+
+                          <div className={style.price}> {item.price}</div>
+                        </p>
+                      </div>
+                    ))}
+
+                  {state === 2 &&
+                    menu.lunch.filter((item)=> item.type === categoria).map((item, indice)=> (
+                      <div className={style.foodPicsContainer}>
+                        <p key={indice}>
+                          {item.name}
+                          <br />
+                          <br />
+
+                          <img src={item.image} alt="foto.png" />
+
+                          <div className={style.price}> {item.price}</div>
+                        </p>
+                      </div>
+                    ))}
+
+                  {state === 3 &&
+                    menu.breakfast.map((item, indice) => (
+                      <div className={style.foodPicsContainer}>
+                        <p key={indice}>
+                          {item.name}
+                          <br />
+                          <br />
+
+                          <img src={item.image} alt="foto.png" />
+
+                          <div className={style.price}> {item.price}</div>
+                        </p>
+                      </div>
+                    ))}
+
+                  {state === 4 &&
+                    menu.breakfast.map((item, indice) => (
+                      <div className={style.foodPicsContainer}>
+                        <p key={indice}>
+                          {item.name}
+                          <br />
+                          <br />
+
+                          <img src={item.image} alt="foto.png" />
+
+                          <div className={style.price}> {item.price}</div>
+                        </p>
+                      </div>
+                    ))}
+                </div>
+              </div>
               <div className={style.receipt}>
-                <div className={style.receiptTitle}>
+                  <div className={style.receiptTitle}>
                   <h2> Receipt</h2>
                   <hr />
                   <div className={style.calculator}></div>
@@ -111,7 +198,7 @@ setInterval(clock, 1000 )*/
                     <button onClick={handleClick} className={style.btnLogout}>
                       Cancel
                     </button>
-                  </div>
+                  </div> </div>
                 </div>
               </div>
             </div>
@@ -122,13 +209,11 @@ setInterval(clock, 1000 )*/
             </button>
           </footer>
         </div>
-      </div>
-    );
-  }
+   
 
-  return (
-    <AuthProvider
-      onUserNotRegistered={firebaseName} /*Componente*/
-    ></AuthProvider>
+    {/*}  <AuthProvider
+        onUserNotRegistered={firebaseName} 
+                    ></AuthProvider>*/}
+    </>
   );
 }
